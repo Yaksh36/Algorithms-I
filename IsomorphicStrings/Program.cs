8230c5818dc7cede75f903ceec7a506cf09c2035
Yaksh Patel <yaksh3636@gmail.com>
2021-03-30 16:37:58 -0500

    Add project files.

---
+++ IsomorphicStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace IsomorphicStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application Start ----------------");
+
+            string[] lines = File.ReadAllLines("C:/Users/Yaksh Patel/Downloads/IsomorphInput1.txt");
+            foreach (string s in lines)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
