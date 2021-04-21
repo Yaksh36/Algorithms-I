692876620399ed8771592fb29a4cc3c334ff6318
Yaksh Patel <yaksh3636@gmail.com>
2021-04-21 15:00:13 -0600

    WeightSort complete

---
+++ NQueens/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace NQueens
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ WeightSort/CustomWeightComparer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WeightSort
+{
+    public class CustomWeightComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            
+            var numberOne = x.Sum(num => int.Parse(num.ToString())); 
+            var numberTwo = y.Sum(num => int.Parse(num.ToString()));
+
+            if (numberOne == numberTwo)
+            {
+                return x.CompareTo(y);
+            }
+            else
+            {
+                return numberOne.CompareTo(numberTwo);
+            }
+
+        }
+    }
+}
